06c49e0327631dd0d4a5d41b12579267adcbeac0
transontri2006 <transontri2006.fpt@gmail.com>
2026-02-07 10:27:54 +0700

    Delete src/lec9_InheritanceAndPolymorphism/Overringding directory

--- src/lec9_InheritanceAndPolymorphism/Overringding/cat.java
+++
@@ -1,9 +0,0 @@
-package lec9_InheritanceAndPolymorphism.Overringding;
-
-public class cat extends Animal {
-
-    @Override
-     void makeSound() {
-        System.out.println("Meows meows");
-    }
-}
